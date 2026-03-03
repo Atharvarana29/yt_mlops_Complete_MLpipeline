e399c2a75e9b9f82f0b93053a080b7a439bb4f53
atharva <atharvarana29@gmail.com>
2026-03-03 13:02:26 +0530

    experimnt tracking using dvc live done

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,6 +5,10 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score , precision_score , recall_score , roc_auc_score
 import logging #module
+from dvclive import Live # it tracks metrics , parameters , plots , training , progress and works with DVC experiments
+import yaml
+
+
 
 ### Implementing Logging Module
 
@@ -34,6 +38,23 @@ if not logger.handlers: # industry best practice to check if not added then add
     logger.addHandler(console_handler) # now first we created the handler and now putting both the handlers inside the logger object 
     logger.addHandler(file_handler)
 
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 # this time we will be loading the data from the processed  file of the data folder , i.e like earlier components from the same last place where our data was last saved 
 def load_model(file_path: str):
@@ -106,11 +127,22 @@ def save_metrics(metrics : dict , file_path : str ) ->None:
     
 def main():
     try:
+        # initiating load params
+        params = load_params(params_path='params.yaml')
+
         clf = load_model('./models/model.pkl') # loading the model
         test_data = load_data('./data/processed/test_tfidf.csv') #loading the test data
         
         X_test = test_data.iloc[:,:-1].values # loading X_test and y_test 
         y_test = test_data.iloc[:,-1].values
+
+# gives options to log lot of things
+        with Live(save_dvc_exp=True) as live: # this params : save_dvc_exp=True : jitne baar bhi hum experiments run karenge ye sabka results record karta jayega
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+
+            live.log_params(params) # logs the params used also
 
         metrics = evaluate_model(clf , X_test , y_test) # calling the evaluate model function
 
